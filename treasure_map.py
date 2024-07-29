ea655725567f0e1e1c69a4da4a4812648fd32d5c
Joseph Ndegwa <45478080+JoeNdegwa@users.noreply.github.com>
2024-07-29 22:21:18 +0300

    Create treasure_map.py

---
+++ treasure_map.py
@@ -0,0 +1,22 @@
+line1 = ["⬜️","️⬜️","️⬜️"]
+line2 = ["⬜️","⬜️","️⬜️"]
+line3 = ["⬜️️","⬜️️","⬜️️"]
+map = [line1, line2, line3]
+print("Hiding your treasure! X marks the spot.")
+position = input() # Where do you want to put the treasure?
+# 🚨 Don't change the code above 👆
+# Write your code below this row 👇
+
+abc = ["a", "b", "c"]
+letter = position[0].lower()
+# print(letter)
+
+letter_index = abc.index(letter)
+#print(letter_index)
+number_index = int(position[1]) - 1
+# print(number_index)
+map[number_index][letter_index] = "X"
+
+# Write your code above this row 👆
+# 🚨 Don't change the code below 👇
+print(f"{line1}\n{line2}\n{line3}")
