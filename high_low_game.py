0e803eed8e0e8454911f3453d7747a289b01b684
Joseph Ndegwa <45478080+JoeNdegwa@users.noreply.github.com>
2024-08-14 17:07:14 +0300

    Create high_low_game.py

---
+++ high_low_game.py
@@ -0,0 +1,13 @@
+"""
+Display Art
+Generate a random account from data
+Format the account data into printable format
+Ask user for a guess
+Check if user is correct
+Get follower count of each account
+Use if statement to check is user is correct
+Give user feedback on their guess
+Keep score
+Make the game repeatable
+Make account at position B become the next account at position A
+"""
