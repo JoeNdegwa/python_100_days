14a02c08d72a607b50552c71e23379492af9c0e8
Joseph Ndegwa <45478080+JoeNdegwa@users.noreply.github.com>
2025-06-30 18:02:42 +0300

    Update longest_harmonious_subsequence.py

--- longest_harmonious_subsequence.py
+++ longest_harmonious_subsequence.py
@@ -14,3 +14,21 @@ class Solution:
                 max_length = max(max_length, i - j + 1)
         
         return max_length
+
+## Solution 2: Using Hashmaps
+from collections import Counter
+
+class Solution:
+    def findLHS(self, nums: list[int]) -> int:
+        # Using HashMaps
+        # Time complexity: O(n)
+        # Space complexity:O(n)
+        frequency_map = Counter(nums)
+        max_length = 0
+
+        for num in frequency_map:
+            if num + 1 in frequency_map:
+                current_length = frequency_map[num] + frequency_map[num + 1]
+                max_length = max(max_length, current_length)
+
+        return max_length
